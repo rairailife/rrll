af77fe6b6a719a693eed05370e5d786549438751
rairailife <yurisyoujo@gmail.com>
2019-09-19 09:30:36 +0900

    create new repository

---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'rairailifelog',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'rairai chan&apos;s life log' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet',  href: 'https://fonts.googleapis.com/css?family=Changa&display=swap' },
+      { rel: 'stylesheet',  type: 'ext/css', href: 'https://unpkg.com/augmented-ui/augmented.css' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="wrapper">
+    <h1 class="title">RaiRai-LifeLog</h1>
+
+    <main class="container">
+      <!-- <app-logo/> -->
+      らいらい！<br>
+      惑星RRLLより通信中。<br>
+      こちらは艦長のらいらいちゃんだ。<br>
+      地上の民よ、我らが蒼き星は健全かね？
+    </main>
+
+    <footer class="footer">
+      <div>
+        This is rairai chan's life log.<br>
+        Linl
+      </div>
+      <span>Copyright &copy; 2019 rairai-chan</span>
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin:  0;
+}
+
+.title::selection, .container::selection {
+  background: #C5545E;
+  color:      #ff00de;
+}
+
+body {
+  background-color: #000;
+  color:            #F3E5E6;
+	text-shadow:      0px 2px 3px #ff00de;
+  font-size:        16px;
+  line-height:      1.5em;
+  display:          flex;
+  justify-content : center;
+  align-items:      center;
+  text-align:       center;
+  min-height:       100vh;
+  width:            100vw;
+}
+
+#wrapper {
+  display:        flex;
+  flex-direction: column;
+  min-height:     100vh;
+  width:          100vw;
+}
+
+h1 {
+  font-size:   32px;
+  line-height: 1.2em;
+}
+
+h2 {
+  font-size:   24px;
+  line-height: 1.2em;
+}
+
+.title {
+  font-family: 'Changa', sans-serif;
+	text-align:  center;
+	margin:      20px auto;
+	color:       #fff;
+	text-shadow: 0 0 10px #C5545E, 0 0 20px #C5545E, 0 0 30px #C5545E, 0 0 40px #ff00de, 0 0 70px #ff00de, 0 0 80px #ff00de, 0 0 100px #ff00de, 0 0 150px #ff00de;
+}
+
+.footer {
+  margin-top:       auto;
+  background-color: #474747;
+	color:            #222;
+	text-shadow:      0px 2px 3px #666;
+}
+.footer::selection {
+  background: #222;
+  color:      #474747;
+}
+</style>
+
