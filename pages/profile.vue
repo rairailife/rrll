1011e06b14732b9f51528b4a69fa0495e0e74def
rairailife <yurisyoujo@gmail.com>
2019-09-19 14:34:25 +0900

    commit

---
+++ pages/profile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <img>
+    <dl>
+      <dt>NAME</dt>
+      <dd>rairai chan</dd>
+
+      <dt>PLANET</dt>
+      <dd>Earth</dd>
+
+      <dt>RACE</dt>
+      <dd>human</dd>
+
+      <dt>LANGUAGE</dt>
+      <dd>Java[Struts2], C#.NET, VB.NET, Javascript, JQuer, Vue.js, Nuxt.js</dd>
+
+      <dt>DATABASE</dt>
+      <dd>Oracle, SQL Server</dd>
+
+      <dt>FINAL FANTASY</dt>
+      <dd>
+        My favorite FINAL FANTASY is IX and my first FINAL FANTASY is it.<br>
+        I want to live in Lindbulm. I want to be a engineer of Flying boat.<br>
+        My second favorite CRISIS CORE.<br>
+        I love Aerith and Zax. I wish for their happiness.<br>
+        When I clear it, I was too cry. The last is saddness.<br>
+        I hold my dream. WE ARE HERO.<br>
+      </dd>
+
+      <dt>JOJO's bizzar adventure</dt>
+      <dd>I love all JOJOs. I especially like Josuke Higashikata above all the JOJOs.</dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
+
