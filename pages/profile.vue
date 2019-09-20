e0736c8782b7d9c50e439eb50b7c9a18b83b64eb
rairailife <yurisyoujo@gmail.com>
2019-09-20 16:47:27 +0900

    プロフィールページを修正

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <img>
+  <div id="wrapper">
+    <h1>Who is Rairai-chan?</h1>
+    <img src="https://placehold.jp/240x240.png">
     <dl>
       <dt>NAME</dt>
       <dd>rairai chan</dd>
@@ -17,6 +18,15 @@
       <dt>DATABASE</dt>
       <dd>Oracle, SQL Server</dd>
 
+      <dt>GitHub</dt>
+      <dd>rairailife</dd>
+
+      <dt>Qiita</dt>
+      <dd>rairailife</dd>
+
+      <dt>twitter</dt>
+      <dd>rairailife</dd>
+
       <dt>FINAL FANTASY</dt>
       <dd>
         My favorite FINAL FANTASY is IX and my first FINAL FANTASY is it.<br>
@@ -30,12 +40,90 @@
       <dt>JOJO's bizzar adventure</dt>
       <dd>I love all JOJOs. I especially like Josuke Higashikata above all the JOJOs.</dd>
     </dl>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Footer
+  }
+}
 </script>
 
 <style>
-</style>
+* {
+  padding: 0;
+  margin:  0;
+}
 
+.title::selection, .container::selection {
+  background: #C5545E;
+  color:      #ff00de;
+}
+
+body {
+  background-color: #000;
+  color:            #F3E5E6;
+	text-shadow:      0px 2px 3px #ff00de;
+  font-size:        16px;
+  line-height:      1.5em;
+  display:          flex;
+  justify-content : center;
+  align-items:      center;
+  text-align:       center;
+  min-height:       100%;
+  width:            100%;
+  box-sizing:       border-box;
+}
+
+#wrapper {
+  display:        flex;
+  flex-direction: column;
+  min-height:     100%;
+  width:          100%;
+  text-align:     center;
+}
+
+img {
+  border-radius: 120px;
+  height:        120px;
+  width:         120px;
+}
+
+h1 {
+  font-size:   32px;
+  line-height: 1.2em;
+}
+
+h2 {
+  font-size:   24px;
+  line-height: 1.2em;
+}
+
+.title {
+  font-family: 'Changa', sans-serif;
+	text-align:  center;
+	margin:      20px auto;
+	color:       #fff;
+	text-shadow: 0 0 10px #C5545E, 0 0 20px #C5545E, 0 0 30px #C5545E, 0 0 40px #ff00de, 0 0 70px #ff00de, 0 0 80px #ff00de, 0 0 100px #ff00de, 0 0 150px #ff00de;
+}
+
+
+          div.demo {
+            display: inline-block;
+            /* width: 110px; */
+            /* height: 110px; */
+            margin: 10px;
+            --aug-border: 5px;
+            --aug-inset: 5px;
+            padding: 10px; /* mind the gap */
+            text-align: center;
+            --aug-border-bg: gold;
+            --aug-inset-bg: gold;
+          }
+</style>
