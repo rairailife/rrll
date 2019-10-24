07ea0ecb558908dae9f6e210907c90cff191bcf3
rai <yurisyoujo@gmail.com>
2019-10-24 15:34:30 +0900

    デフォルトレイアウト追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,43 @@
 <template>
-  <div id="wrapper">
-    <h1 class="title">RaiRai-LifeLog</h1>
+  <div><!-- id="wrapper" -->
+    <Header/>
     <Menu/>
-    <main class="articleList">
+    <!-- <main class="articleList"> -->
       <nuxt><!-- ここにlayoutを設定したpagesが表示される --></nuxt>
-    </main>
+    <!-- </main> -->
 
     <Footer/>
   </div>
 </template>
 
 <script>
-import Footer from '~/components/Footer.vue'
+import Header from '~/components/Header.vue'
 import Menu from '~/components/Menu.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
-    Footer,
-    Menu
+    Header,
+    Menu,
+    Footer
   }
 }
 </script>
 
 <style scoped>
-* {
-  padding: 0;
-  margin:  0;
-  height:           100%;
-  /* min-height:       100vh; */
-  width:   100%;
-}
-*::selection {
-  background: #C5545E;
-  color:      #ff00de;
-}
-
-#wrapper {
+/* #wrapper {
   padding: 0;
   margin:  0;
   font-size:        16px;
   line-height:      1.5em;
   box-sizing:       border-box;
-}
-
-h1 {
-  font-size:   32px;
-  line-height: 1.2em;
-}
-
-.title {
-	text-align:  center;
-	text-shadow: 0px 2px 3px #ff00de;
-  font-family: 'Changa', sans-serif;
-}
+} */
 /*
 .articleList {
   margin-bottom: 50px;
 } */
+
+header {
+  margin-top:  .5em;
+}
 </style>
