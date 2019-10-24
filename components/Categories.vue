362f2157141bdfb2eb06a65589e52dbe1ac1f78b
佐藤 みらい <m-satou@dom.onthemark.co.jp>
2019-10-24 14:50:18 +0900

    フッターのカテゴリリストコンポーネント追加

---
+++ components/Categories.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="flexbox">
+    <li v-for="(category, index) in categories" :key=index>
+      <a href="">{{ category.title }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        {title: 'プログラミング', url: ''},
+        {title: '英語', url: ''},
+        {title: '趣味', url: ''},
+        {title: '日常', url: ''},
+        {title: '業務連絡', url: ''},
+        {title: '未分類', url: ''},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ul {
+  display:   flex;
+}
+
+li {
+  margin:      5px;
+  font-size:   13px;
+  white-space: nowrap;
+  text-align:  center;
+}
+li a {
+  display:     block;
+  font-weight: bold;
+  color:       #222;
+  text-shadow: 1px 1px 1px #666;
+  padding:     5px;
+  border:      1px solid #666;
+  box-shadow:  1px 1px 1px #222, 1px 1px 1px #222 inset;
+}
+</style>
