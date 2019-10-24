8f140462cad96cabc8a3a9480c107523e9160cb4
rai <yurisyoujo@gmail.com>
2019-10-24 15:32:54 +0900

    フッターコンポーネント追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,108 +1,97 @@
 <template>
   <footer class="footer">
-    <div class="gradation"></div>
     <div class="categories">
       <h3>Categories</h3>
-      <ul v-for="(category, index) in categories" :key=index>
-        <li>{{ category.title }}</li>
-      </ul>
-    </div>
-    <div class="contacts">
-      <h3>Contacts</h3>
-      連絡を取りたいのなら...
-    </div>
-    <div class="links">
-      <h3>LinKs</h3>
-      <a href="https://sai-tsurumi.netlify.com">sai</a>
+      <Categories />
     </div>
     <div class="author">
       <h3>Author</h3>
       らいらいちゃん<br>
       How are you!?&nbsp;<nuxt-link to="/profile">...more&gt;</nuxt-link>
     </div>
-    <span class="copy">Copyright &copy; 2019 rairai-chan</span>
+    <div class="links">
+      <h3>LinKs</h3>
+      <a href="https://sai-tsurumi.netlify.com">sai</a>
+    </div>
+    <div class="copy">Copyright &copy; 2019 rairai-chan</div>
   </footer>
 </template>
 
 <script>
+import Categories from '~/components/Categories.vue'
+
 export default {
-  data () {
-    return {
-      categories: [
-        {title: 'かてごりー１', url: ''},
-        {title: 'かてごりー２', url: ''},
-        {title: 'かてごりー３', url: ''},
-        {title: 'かてごりー４', url: ''},
-        {title: 'かてごりー５', url: ''},
-        {title: 'かてごりー６', url: ''},
-        {title: 'かてごりー７', url: ''},
-        {title: 'かてごりー８', url: ''},
-        {title: 'かてごりー９', url: ''},
-        {title: 'かてごりー０', url: ''},
-      ]
-    }
+  components: {
+    Categories
   }
 }
 </script>
 
 <style scoped>
-/* * {
-  padding:    0;
-  margin:     0;
-  width:      100%;
-}
-*/
-
 ::selection {
   background: #222;
   color:      #474747;
 }
+
 .footer {
   background-color: #474747;
   color:            #666;
-  width:            100%;
   box-sizing:       border-box;
-/*
-  height:           100%;
-  min-height:       100%;
-  margin-top:       auto;
-  padding-bottom:   0;
-  */
-}
-.gradation {
-  height:     50px;
-  background: linear-gradient(#222222, #474747);
-}
-
-.footer div {
-  padding:       0 50px;
-  margin-bottom: 2em;
+  padding:          0 50px;
+  font-size:        13px;
 }
 
 .footer h3 {
   color:          #474747;
+  font-family:    'Changa', sans-serif;
   text-shadow:    -1px -1px 1px #222, 1px 1px #666;
   line-height:    2em;
   letter-spacing: 3px;
+  margin-top:     .8em;
+  font-size:      20px;
 }
-
-/*
-ul {
-  list-style: none;
-	margin:     20px;
-	padding:    0;
-  display:    flex;
-}
-li {
-	padding: 8px 20px 6px;
-	border:  1px solid #ddd;
-}
-li:not(:first-child) {
-	border-left: none;
-} */
 
 .copy {
-  color: #222;
+  font-family:      'Changa', sans-serif;
+  margin:           0 -50px;
+  margin-top:       .8em;
+  background-color: #222;
+  width:            calc(100% + 100px);
+	text-align:       center;
+}
+
+.links a {
+  color:        #222;
+  font-weight:  bold;
+  text-shadow:  1px 1px 1px #666;
+  position:     relative;
+  padding-left: 22px;
+}
+.links a::before {
+  position:      absolute;
+  content:       '';
+  width:         17px;
+  height:        17px;
+  background:    #474747;
+  border-radius: 50%;
+  box-shadow:    1px 1px 1px #666 inset, 1px 1px 1px #222;
+  top:           50%;
+  left:          0;
+  margin-top:    -7px;
+}
+.links a::after {
+  position:          absolute;
+  content:           '';
+  width:             5px;
+  height:            5px;
+  border-top:        solid 1px #222;
+  border-right:      solid 1px #222;
+  -webkit-transform: rotate(45deg);
+  transform:         rotate(45deg);
+  top:               50%;
+  left:              5px;
+  margin-top:        -1px;
 }
 </style>
+
 
