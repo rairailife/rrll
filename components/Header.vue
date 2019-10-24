81c080f648f778de939f24d231c80d321fd6d107
佐藤 みらい <m-satou@dom.onthemark.co.jp>
2019-10-24 14:54:24 +0900

    ヘッダーコンポーネント追加

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <span>{{ description }}</span>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'RaiRai-LifeLog',
+      description: 'Best day of my life!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+	text-align:  center;
+}
+
+h1 {
+	text-shadow: 0px 2px 3px #ff00de;
+  font-family: 'Changa', sans-serif;
+}
+
+span {
+  font-family: 'Changa', sans-serif;
+	color:       #ff00de;
+}
+</style>
+
